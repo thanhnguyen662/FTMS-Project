5d2749c7dfa47bdfccc7c26ad77a8afd8f35c630
thinvv <kiept@DESKTOP-S43NLL5>
2020-10-18 18:23:25 +0700

    v1

---
+++ FTMS/Controllers/AdminController.cs
@@ -0,0 +1,125 @@
+﻿using FTMS.Models;
+using FTMS.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FTMS.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext context;
+        public AdminController()
+		    {
+           context = new ApplicationDbContext();
+		    }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var userInfor = (from user in context.Users
+                       select new
+                       {
+                         UserId = user.Id,
+                         Username = user.UserName,
+                         EmailAddress = user.Email,
+                         RoleName = (from userRole in user.Roles
+                                     join role in context.Roles
+                                     on userRole.RoleId
+                                     equals role.Id
+                                     select role.Name)
+                       }
+                       ).ToList().Select(p => new UserRoleViewModel()
+                       {
+                         UserId = p.UserId,
+                         Username = p.Username,
+                         Email = p.EmailAddress,
+                         Role = string.Join(",", p.RoleName)
+                       }
+                       );
+           return View(userInfor);
+        }
+        //DELETE ACCOUNT
+        [HttpGet]
+        public ActionResult Delete(string id)
+        {
+            var AccountInDB = context.Users.SingleOrDefault(p => p.Id == id);
+
+            if (AccountInDB == null)
+            {
+                return HttpNotFound();
+            }
+
+            context.Users.Remove(AccountInDB);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Edit 
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            var AccountInDB = context.Users.SingleOrDefault(p => p.Id == id);
+            if (AccountInDB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(AccountInDB);
+        }
+
+        //EDIT
+        [HttpPost]
+        public ActionResult Edit(ApplicationUser user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var AccountInDB = context.Users.SingleOrDefault(p => p.Id == user.Id);
+
+            if (AccountInDB == null)
+            {
+                return HttpNotFound();
+            }
+
+            AccountInDB.UserName = user.UserName;
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        //RESET PASSWORD
+        [HttpGet]
+        public ActionResult Reset(string id)
+        {
+            var AccountInDB = context.Users.SingleOrDefault(p => p.Id == id);
+
+            if (AccountInDB == null)
+            {
+                return HttpNotFound();
+            }
+
+            /*context.Users.Remove(AccountInDB);*/
+            var userId = System.Web.HttpContext.Current.User.Identity.GetUserId();
+            userId = AccountInDB.Id;
+            if (userId != null)
+            {
+                UserManager<IdentityUser> userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>());
+                userManager.RemovePassword(userId);
+                String newPassword = "1234567";
+                userManager.AddPassword(userId, newPassword);
+            }
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FTMS/Startup.cs
@@ -0,0 +1,82 @@
+﻿using FTMS.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FTMS.Startup))]
+namespace FTMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+    // In this method we will create default User roles and Admin user for login    
+    private void createRolesandUsers()
+    {
+      ApplicationDbContext context = new ApplicationDbContext();
+
+      var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+      var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+      // In Startup iam creating first Admin Role and creating a default Admin User     
+      if (!roleManager.RoleExists("Admin"))
+      {
+
+        // first we create Admin rool    
+        var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+        role.Name = "Admin";
+        roleManager.Create(role);
+
+        //Here we create a Admin super user who will maintain the website                   
+
+        var user = new ApplicationUser();
+        user.UserName = "Admin";
+        user.Email = "Admin@gmail.com";
+
+        string userPWD = "Abc@123";
+
+        var chkUser = UserManager.Create(user, userPWD);
+
+        //Add default User to Role Admin    
+        if (chkUser.Succeeded)
+        {
+          var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+        }
+      }
+
+      // Creating Training Staff role     
+      if (!roleManager.RoleExists("Training Staff"))
+      {
+        var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+        role.Name = "Training Staff";
+        roleManager.Create(role);
+
+      }
+
+      // Creating Trainer role     
+      if (!roleManager.RoleExists("Trainer"))
+      {
+        var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+        role.Name = "Trainer";
+        roleManager.Create(role);
+
+      }
+
+      // Creating Trainee role     
+      if (!roleManager.RoleExists("Trainee"))
+      {
+        var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+        role.Name = "Trainee";
+        roleManager.Create(role);
+
+      }
+    }
+  }
+}
